1857d2799cd810ec39f747976283914a54b09d37
boomcrash <alexmol_14@outlook.com>
2023-10-17 15:31:36 -0500

    activando cors

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configurar opciones de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 # Clase modelo Pydantic para un dinosaurio
 class Dinosaurio(BaseModel):
